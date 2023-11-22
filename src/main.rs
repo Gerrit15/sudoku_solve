5e1f27e6785ade6a15d115ce3f7704ec672d87d4
gerrit <overlorde159@gmail.com>
2023-11-22 09:14:24 -0500

    housekeeping, notes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-//plan: Serde example.csv in -> csv crate, print it all out
-//plan isn't so hot, dump Serde? dunno.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+// Thoughts: 
+//  - errors are not really handled much
+//      - that includes making sure the file and format is correct
+//      - that includes clap parameters
+//  - serde is gonna take some work
+//  - managing column and row groups shouldn't be difficult, but the square
+//    groups is going to require finesse
+
 use std::fs::File;
-use clap::Parser;
 use std::path::PathBuf;
+use clap::Parser;
 
 fn main () {
     let args = Args::parse();
+
     let path = args.path.to_str().unwrap().to_owned();
     let file = File::open(path).unwrap();
+
+    //load in a CSV, use crate to unwrap it, pull out rows, push them into items at Tiles
     let mut rdr = csv::ReaderBuilder::new().has_headers(args.contains_header).from_reader(file);
     let mut items = vec![];
     for result in rdr.deserialize() {
@@ -22,7 +30,15 @@ fn main () {
         }
         items.push(row);
     }
+
     let board = Board{items};
+    let mut ok = true;
+    if board.items.len() != 9 {ok = false}
+    for i in &board.items {
+        if i.len() != 9 {ok = false}
+    };
+
+    println!("Is board correct? {}", ok);
     board.display();
 }
 
@@ -43,10 +59,13 @@ impl Board {
     }
 }
 
+//This allows each tile to either be in a single state or ... more
 enum Tile {
     Num(u8),
     Non(Vec<u8>)
 }
+
+//Clap arguements. 
 
 ///TODO: description
 #[derive(Parser)]
