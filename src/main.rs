be05f8889800e33346bc7921647a4e79c908ccf2
Gerrit15 <overlorde159@gmail.com>
2023-11-28 15:26:38 -0500

    added display and new functions to board. Something doesn't quite work, display shows nothing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
 fn main() {
     let path = "example.csv".to_owned();
     let board = load_board(path);
-
-    for i in board {
-        for j in i {
-            print!("{}, ", j);
-        }
-        println!();
-    }
+    board.display();
 }
 
 //this function takes a file system path, and returns a 2d vec of strings if it's a properly formatted
 //CSV, using the aptly named CSV crate. This is extremely volitile, and cannot handle improperly
 //formatted files
-fn load_board(path: String) -> Vec<Vec<String>> {
+fn load_board(path: String) -> Board {
     let file = std::fs::File::open(path).unwrap();
 
     //let "has headers" be a command line option
@@ -23,11 +17,12 @@ fn load_board(path: String) -> Vec<Vec<String>> {
         let record: Vec<String> = i.unwrap();
         board.push(record);
     }
-    return board
+    Board::new(board)
 }
 
 //Num for when a tile has a single state, to allow for faster iteration
 //Non for when a tile's entropy is not zero
+//consider in future using box and making it recursive
 enum Tile {
     Num(u8),
     Non(Vec<u8>)
@@ -36,4 +31,34 @@ enum Tile {
 //a simple 2d data structure to hold the sudoku board
 struct Board {
     items: Vec<Vec<Tile>>
+}
+
+impl Board {
+    fn new(items: Vec<Vec<String>>) -> Board {
+        let mut board = vec![];
+        for i in items {
+            for j in i {
+                let mut row = vec![];
+                match j.parse::<u8>() {
+                    Ok(x) => row.push(Tile::Num(x)),
+                    //1, 2, 3 is placeholder
+                    Err(_) => row.push(Tile::Non(vec![1, 2, 3]))
+                }
+            }
+        }
+        Board {
+            items: board
+        }
+    }
+    fn display(&self) {
+        for i in &self.items {
+            for j in i {
+                match j {
+                    Tile::Num(x) => print!("{x}, "),
+                    Tile::Non(x) => print!("{:?}, ", x)
+                }
+            }
+        }
+        println!();
+    }
 }
