dd704e3eff9926232155cf7c8dc374366521c4c9
Gerrit15 <overlorde159@gmail.com>
2023-11-29 09:46:32 -0500

    begun to handle errors, starting with path, file

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,25 @@ use clap::Parser;
 
 fn main() {
     let args = Args::parse();
-    let board = load_board(args);
+    let board = load_board(args).unwrap();
     board.display();
 }
 
 //this function takes a file system path, and returns a 2d vec of strings if it's a properly formatted
 //CSV, using the aptly named CSV crate. This is extremely volitile, and cannot handle improperly
 //formatted files
-fn load_board(args: Args) -> Board {
-    let path = args.path.to_str().unwrap().to_owned();
-    let file = std::fs::File::open(path).unwrap();
+fn load_board(args: Args) -> Result<Board, String> {
+
+    let path = match args.path.to_str() {
+        Some(x) => x.to_owned(),
+        None => return Err("Failed to parse path".to_owned())
+    };
+
+    //let file = std::fs::File::open(path).unwrap();
+    let file = match std::fs::File::open(path) {
+        Ok(x) => x,
+        Err(x) => return Err(x.to_string())
+    };
 
     //let "has headers" be a command line option
     let mut csv_reader = csv::ReaderBuilder::new().has_headers(args.contains_header).from_reader(file);
@@ -23,7 +32,7 @@ fn load_board(args: Args) -> Board {
         let record: Vec<String> = i.unwrap();
         board.push(record);
     }
-    Board::new(board)
+    Ok(Board::new(board))
 }
 
 ///TODO: command line description
@@ -41,6 +50,7 @@ struct Args {
     verbose: bool
 }
 
+//atn: refactoring Args is not worth it. Parser does not like fields being public
 //Other potential options:
 //  - Creative: tries more to load numbers
 //  - Strict: does not? Might be redundant
