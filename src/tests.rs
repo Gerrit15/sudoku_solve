866852a3f8428e8fa7865939fe62412e93b32b67
Gerrit15 <overlorde159@gmail.com>
2023-12-01 09:13:57 -0500

    gen now does attempts flag

--- src/tests.rs
+++ src/tests.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 //To make sure a basic board is loaded in properly
 #[test]
 fn base_load() {
-    let args = arg_gen("test_csvs/base.csv", false);
+    let args = arg_gen("test_csvs/base.csv", false, false);
     let board = load_board(args);
     assert!(matches!(board, Ok(_)));
 }
@@ -13,7 +13,7 @@ fn base_load() {
 #[test]
 fn base_non() {
     use Tile::*;
-    let args = arg_gen("test_csvs/non.csv", false);
+    let args = arg_gen("test_csvs/non.csv", false, false);
     let board = load_board(args).unwrap();
     let b2 = Board {
         //This is to make sure that the Non is placed in the right spot
@@ -35,7 +35,7 @@ fn base_non() {
 //To confirm files with headers can be loaded
 #[test]
 fn header() {
-    let args = arg_gen("test_csvs/header.csv", true);
+    let args = arg_gen("test_csvs/header.csv", true, false);
     let board = load_board(args);
     assert!(matches!(board, Ok(_)));
 }
@@ -43,7 +43,7 @@ fn header() {
 //To confirm that a board that's too tall (too many rows) isn't loaded in
 #[test]
 fn too_tall() {
-    let args = arg_gen("test_csvs/long.csv", false);
+    let args = arg_gen("test_csvs/long.csv", false, false);
     let board = load_board(args);
     assert_eq!(board, Err("Board is not 9 rows".to_string()))
 }
@@ -51,7 +51,7 @@ fn too_tall() {
 //To confirm that a board that's too short (not enough rows) isn't loaded in
 #[test]
 fn too_short() {
-    let args = arg_gen("test_csvs/short.csv", false);
+    let args = arg_gen("test_csvs/short.csv", false, false);
     let board = load_board(args);
     assert_eq!(board, Err("Board is not 9 rows".to_string()))
 }
@@ -59,7 +59,7 @@ fn too_short() {
 //To confirm that a board that's too wide (too many columns) isn't loaded
 #[test]
 fn too_wide() {
-    let args = arg_gen("test_csvs/wide.csv", false);
+    let args = arg_gen("test_csvs/wide.csv", false, false);
     let board = load_board(args);
     assert_eq!(board, Err("Board is not 9 columns".to_string()))
 }
@@ -67,7 +67,7 @@ fn too_wide() {
 //To confirm that a board that's too narrow (not enough columns) isn't loaded
 #[test]
 fn too_narrow() {
-    let args = arg_gen("test_csvs/narrow.csv", false);
+    let args = arg_gen("test_csvs/narrow.csv", false, false);
     let board = load_board(args);
     assert_eq!(board, Err("Board is not 9 columns".to_string()))
 }
@@ -75,7 +75,7 @@ fn too_narrow() {
 //To confirm that a board that contains numbers greater then 9 isn't loaded
 #[test]
 fn out_of_bounds() {
-    let args = arg_gen("test_csvs/out_of_bound.csv", false);
+    let args = arg_gen("test_csvs/out_of_bound.csv", false, false);
     let board = load_board(args);
     assert_eq!(board, Err("Board contains invalid number".to_string()))
 }
@@ -83,17 +83,17 @@ fn out_of_bounds() {
 //To confirm that a board with non uniform width isn't loaded
 #[test]
 fn non_uniform() {
-    let args = arg_gen("test_csvs/non_uniform.csv", false);
+    let args = arg_gen("test_csvs/non_uniform.csv", false, false);
     let board = load_board(args);
     assert_eq!(board, Err("CSV error: record 1 (line: 2, byte: 18): found record with 7 fields, but the previous record has 9 fields".to_string()))
 }
 
 //A simple function to generate example cmd line arguements
-fn arg_gen(path: &str, header:bool) -> Args {
+fn arg_gen(path: &str, header:bool, attempt: bool) -> Args {
     Args {
         path: PathBuf::from(path),
         contains_header: header,
         verbose: false,
-        attempt: false
+        attempt: attempt
     }
 }
