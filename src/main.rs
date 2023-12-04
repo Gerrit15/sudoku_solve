169cd9fd3161327c8c60414c9ec3d4d9defce480
Gerrit15 <overlorde159@gmail.com>
2023-12-04 09:28:07 -0500

    implemented Error struct in main

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,26 @@ fn main() {
     let result = run();
     match result {
         Ok(()) => (),
-        Err(e) => println!("{}", e)
+        Err(e) => println!("{}", e.message)
     }
 }
 
 //The reason for a run function is that using return statements can cause the function to return
 //prematurely, meaning that code doesn't get increasing nested. Otherwise, this functions nearly
 //identical to main
-fn run() -> Result<(), String> {
+fn run() -> Result<(), Error> {
     let args = Args::parse();
     let board = match Board::load_board(args.clone()){
         Ok(b) => b,
         Err(e) => {
-            println!("Oops! Couldn't load CSV!");
-            return Err(e)
+            let line = line!()-3;
+            let file = file!().to_string();
+            let message = match args.verbose {
+                true => "Oops! Couldn't load CSV!\n".to_string() + &e,
+                false => "Oops! Couldn't load CSV!".to_string()
+            };
+            //println!("Oops! Couldn't load CSV!");
+            return Err(Error::new(message, line, file))
         }
     };
     if args.verbose {board.display()}
