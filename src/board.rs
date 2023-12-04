6e532b225a36abbbcb606246204aea812e9a806f
Gerrit15 <overlorde159@gmail.com>
2023-12-04 09:35:52 -0500

    implemented Error struct in board

--- src/board.rs
+++ src/board.rs
@@ -13,16 +13,22 @@ pub struct Board {
     pub items: Vec<Vec<Tile>>
 }
 
+use super::Error;
 impl Board {
     //generates a new Board given a 2d Vec of Strings
-    pub fn new(items: Vec<Vec<String>>, attempt: bool) -> Result<Board, String> {
+    pub fn new(items: Vec<Vec<String>>, attempt: bool) -> Result<Board, Error> {
         let mut board = vec![];
         for i in items {
             let mut row = vec![];
             for j in i {
                 match j.parse::<u8>() {
                     Ok(x) => {
-                        if x > 9 || x == 0 {return Err("Board contains invalid number".to_string())}
+                        if x > 9 || x == 0 {
+                            let line = line!()-1;
+                            let file = file!().to_string();
+                            let message = "Board contains invalid number".to_string();
+                            return Err(Error::new(message, line, file))
+                        }
                         row.push(Tile::Num(x))
                     },
                     Err(_) => {
@@ -46,9 +52,19 @@ impl Board {
             }
             board.push(row);
         }
-        if board.len() != 9 {return Err("Board is not 9 rows".to_string())}
+        if board.len() != 9 {
+            let line = line!()-1;
+            let file = file!().to_string();
+            let message = "Board is not 9 rows".to_string();
+            return Err(Error::new(message, line, file))
+        }
         //It is given by the CSV crate that every row is the same length
-        if board[0].len() != 9 {return Err("Board is not 9 columns".to_string())}
+        if board[0].len() != 9 {
+            let line = line!()-1;
+            let file = file!().to_string();
+            let message = "Board is not 9 columns".to_string();
+            return Err(Error::new(message, line, file))
+        }
 
         Ok(Board {
             items: board
